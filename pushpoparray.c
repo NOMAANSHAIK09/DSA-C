bee4a42f64fbd3c1e7a64cf88ac550db8287fbb2
NOMAANSHAIK09 <nomaanshaiknomaanshaik@gmail.com>
2025-03-13 15:33:54 +0530

    Add files via upload

---
+++ pushpoparray.c
@@ -0,0 +1,51 @@
+
+#  include <stdio.h>
+int st[5] = {0};
+int stkpointer = -1;
+void push( int element)
+{
+    if(stkpointer < 4)
+    {
+
+        stkpointer++;
+        st[stkpointer] = element;
+        printf(" %d element inserted\n", element);
+        }
+       else
+       {
+           printf("over flow\n");
+       }
+}
+       int pop()
+       {
+           int x;
+           if(stkpointer>-1)
+           {
+              x = st[stkpointer];
+              stkpointer--;
+               printf(" %d element is poped\n", x);
+               return x;
+           }
+           else
+           {
+               printf("down flow\n");
+               return -1;
+       }
+       }
+       int main()
+       {
+           printf(" element added :\n");
+           push(10);
+           push(15);
+           push(16);
+           push(18);
+           push(90);
+
+           printf("element popped:\n");
+           printf("%d \n", pop());
+           printf("%d \n", pop());
+           printf("%d \n", pop());
+           printf("%d \n", pop());
+           printf("%d \n", pop());
+           return 0;
+       }
